b119a8458ebf3febbea007fe60ac0f44d9d16ef9
lksuenfa <70963322+lksuenfa@users.noreply.github.com>
2021-10-15 20:50:09 -0400

    change json

--- exercise 2/readme.md
+++ exercise 2/readme.md
@@ -1,2 +1,54 @@
 CART351 Exercise 2
 Leanne Suen Fa
+
+"Saturday": [
+{
+"time": "8:25 PM",
+"description": "Icarus streaming",
+"category": "Game",
+"activity": "leisure"
+},
+
+    {
+      "time": "8:30 PM",
+      "description": "Light novel translations",
+      "category": "Book",
+      "activity": "leisure"
+    },
+
+    {
+      "time": "8:59 PM",
+      "description": "Peach resin dessert",
+      "category": "Food",
+      "activity": "cooking"
+    },
+
+    {
+      "time": "9:49 PM",
+      "description": "DIY cosmetics recipes",
+      "category": "Crafting",
+      "activity": "leisure"
+    },
+
+    {
+      "time": "10:22 PM",
+      "description": "Salmon miso butter",
+      "category": "Food",
+      "activity": "cooking"
+    },
+    {
+      "time": "10:55 PM",
+      "description": "Simple Polygon Generator",
+      "category": "Blender",
+      "activity": "studying"
+    },
+
+    {
+      "time": "10:22 PM",
+      "description": "Icarus game sleep",
+      "category": "Game",
+      "activity": "leisure"
+    }
+
+]
+}
--- exercise 2/script.js
+++ exercise 2/script.js
@@ -1,23 +1,43 @@
 /*
 Description of exercise 2
 */
+let searchItem;
+let firstTime = true;
+let container = $("#result-container");
 
 $(document).ready(function () {
   // Get search terms
   $("#searchButton").click(getSearchTerms);
-
   function getSearchTerms() {
     // deletes text in search bar
     $("#result-container").empty();
     // get value from input field
-    let searchItem = $("#searchText").val();
-    console.log(searchItem);
-  }
+    searchItem = $("#searchText").val();
+    console.log("searched for : " + searchItem);
 
-  // get JSON
-  let data = $.getJSON("assets/data.json", function () {
-    console.log("data loaded");
-  }).fail(function () {
-    console.log("error");
-  });
-});
+    // function to load JSON
+    $.getJSON(
+      "assets/data.json",
+      displayResults
+      // failure to load JSON
+    ).fail(function () {
+      console.log("error");
+    }); //fail
+  } //getSearchTerms
+}); //.ready
+
+// function to display results
+function displayResults(results) {
+  // if (firstTime) {
+  //   let firstMessage = $("<p>").appendTo(container);
+  //   firstMessage.html("hi");
+  //   firstTime = false;
+  // }
+
+  // successfully load JSON
+  dataFromJSON = results;
+  console.log(dataFromJSON);
+
+  let a = $("<p>").appendTo(container);
+  a.html(dataFromJSON.searchItem);
+} //displayResults
