ac695e2438f00970280d2d267098640e3485a6bf
lksuenfa <70963322+lksuenfa@users.noreply.github.com>
2021-10-22 18:41:08 -0400

    trying to debug

--- exercise 2/Square.js
+++ exercise 2/Square.js
@@ -1,16 +1,15 @@
 class Square {
-  constructor(size, canvasWidth, canvasHeight, context) {
+  constructor(canvasWidth, canvasHeight, context) {
     this.canvasWidth = canvasWidth;
     this.canvasHeight = canvasHeight;
 
     // create a margin where squares won't appear
     this.x = 50 + Math.floor(Math.random() * (this.canvasWidth - 100));
     this.y = 50 + Math.floor(Math.random() * (this.canvasHeight - 100));
-    this.size = size;
-    this.context = context;
 
-    this.speed = 1;
-    this.colorChoice = `rgb(255,0,0)`;
+    this.size = 10;
+    this.context = context;
+    this.colorChoice = `rgb(255,0,0)`; //original red colour
   }
   display() {
     this.context.fillStyle = this.colorChoice;
@@ -18,31 +17,30 @@ class Square {
   }
 
   grow() {
-    this.size = this.size * 5;
+    this.size = this.size * 3;
     this.context.fillRect(this.x, this.y, this.size, this.size);
   }
-  shrink() {
-    this.size = 10;
-    this.context.fillRect(this.x, this.y, this.size, this.size);
-  }
+
   displayGreen() {
-    this.context.fillStyle = `rgb(0,255,0)`;
+    this.context.fillStyle = `rgb(0,255,0)`; //green
     this.context.fillRect(this.x, this.y, this.size, this.size);
   }
 
   displayBlue() {
-    this.context.fillStyle = `rgb(66, 135, 245)`;
+    this.context.fillStyle = `rgb(66, 135, 245)`; //blue
     this.context.fillRect(this.x, this.y, this.size, this.size);
   }
 
   displayYellow() {
-    this.context.fillStyle = `rgb(245, 200, 66)`;
+    this.context.fillStyle = `rgb(245, 200, 66)`; //yellow
     this.context.fillRect(this.x, this.y, this.size, this.size);
   }
   displayMint() {
-    this.context.fillStyle = `rgb(66, 245, 227)`;
+    this.context.fillStyle = `rgb(66, 245, 227)`; //mint
     this.context.fillRect(this.x, this.y, this.size, this.size);
   }
+
+  // reset to original colour and size
   reset() {
     this.size = 10;
     this.colorChoice = `rgb(255,0,0)`;
--- exercise 2/script.js
+++ exercise 2/script.js
@@ -2,12 +2,12 @@
 Description of exercise 2
 */
 let searchItem;
-let firstTime = true;
 let container = $("#result-container");
 let canvas;
 let data;
 
 let displayResult = {
+  day: undefined,
   description: undefined,
   time: undefined,
   category: undefined,
@@ -16,8 +16,6 @@ let displayResult = {
 let context;
 let filteredResults;
 let squares = [];
-let squareSize = 10;
-let count = 0;
 
 window.onload = function () {
   setUpCanvas();
@@ -53,12 +51,7 @@ function setUpCanvas() {
 
   // draw squares
   for (let i = 0; i < data.activities.length; i++) {
-    let squareImg = new Square(
-      squareSize,
-      canvas.width,
-      canvas.height,
-      context
-    );
+    let squareImg = new Square(canvas.width, canvas.height, context);
     squares.push(squareImg);
   }
   for (let i = 0; i < data.activities.length; i++) {
@@ -69,29 +62,26 @@ function setUpCanvas() {
 // function to display results
 function displayResults(results) {
   data = results;
-  searchAndFilter();
-  // success
-}
-
-// function to search and filter results
-function searchAndFilter() {
-  // Get search terms
+  // On click get search terms
   $("#searchButton").click(getSearchTerms);
 
   function getSearchTerms() {
     // deletes text in search bar
     $("#result-container").empty();
+
     // get value from input field
     searchItem = $("#searchText").val();
 
+    // clear canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
 
+    // reset squares
     for (let i = 0; i < squares.length; i++) {
       squares[i].reset();
       squares[i].display();
     }
 
-    // filter
+    // filter results
     filteredResults = data["activities"].filter(function (json) {
       return json.day === searchItem;
     });
@@ -109,8 +99,15 @@ function searchAndFilter() {
 
       // display results
       let jsonData = data.activities;
+
       for (let i = 0; i < filteredResults.length; i++) {
-        let singleResultContainer = $("<div>");
+        console.log(searchItem === jsonData[i].day);
+        // if (searchItem === jsonData[i].day) {
+        displayResult.day = $("<p>")
+          .appendTo(container)
+          .addClass("search_result_day");
+        displayResult.day.text(jsonData[i].day);
+
         displayResult.description = $("<p>")
           .appendTo(container)
           .addClass("search_result_description");
@@ -124,11 +121,10 @@ function searchAndFilter() {
         displayResult.category = $("<p>")
           .appendTo(container)
           .addClass("search_result_category");
-        displayResult.category.text(jsonData[i].category);
+        displayResult.category.text("Category: " + jsonData[i].category);
 
         // modify square appearance
         squares[i].grow();
-        console.log(filteredResults.length);
 
         // change colour according to category
         if (filteredResults[i].category === "Game") {
@@ -139,8 +135,9 @@ function searchAndFilter() {
           squares[i].displayYellow();
         } else if (filteredResults[i].category === "Book") {
           squares[i].displayMint();
-        } //if
+          // }
+        }
       }
     } //fruitful search
-  } //getSearchTerms
+  }
 }
